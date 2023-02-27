38f5d315a573a8ecd3e36b3706bdb28625ed31a4
Dallas Palumbo <palumbodallas@gmail.com>
2023-02-27 18:52:05 -0500

    express ya self

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
+const express = require("express");
+const app = express();
+const port = 3000;
 
+app.get('/', (req,res) =>{
+    res.send("hello world")
+})
 
+app.listen(4000, () =>{
+    console.log("app running on port 4000")
+})
 
 
 const apiEndpoint = "https://the-trivia-api.com/api/questions?limit=10&region=US&difficulty=hard"
